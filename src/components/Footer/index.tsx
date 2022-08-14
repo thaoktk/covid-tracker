0cc5f7f7e57b4f968758346f4bad5887e6bdb42c
Thaoktk <tranthuthao.bhn.96@gmail.com>
2022-08-14 19:12:02 +0700

    add padding footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="mt-4">
+    <footer className="mt-4 py-5">
       <p className="text-center text-lg font-semibold">
         Development by <code className="text-main-red">thaoktk - CTer</code>{" "}
         with ❤️
