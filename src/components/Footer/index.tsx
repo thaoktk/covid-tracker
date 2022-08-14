b22bd76fc793fba05598e4a80391177fae6e9e8b
Thaoktk <tranthuthao.bhn.96@gmail.com>
2022-08-14 19:10:00 +0700

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className="mt-4">
+      <p className="text-center text-lg font-semibold">
+        Development by <code className="text-main-red">thaoktk - CTer</code>{" "}
+        with ❤️
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
